7b6f674c6dea25cec38260330ce31ec80f5ce246
raythurman2386 <raymondthurman5@gmail.com>
2023-01-26 14:20:56 -0600

    mouse automation script

---
+++ mouse_keyboard_automation/main.py
@@ -0,0 +1,24 @@
+# mouse automation
+import pyautogui as pg
+import time
+
+
+# Function that moves the mouse to the 4 corners of the screen
+def move_mouse():
+    pg.moveTo(100, 100, duration=1)
+    pg.click(100,100)
+    pg.moveTo(1820, 100, duration=1)
+    pg.moveTo(100, 980, duration=1)
+    pg.click(100, 980)
+    pg.moveTo(1820, 980, duration=1)
+
+
+# With 160 revolutions over 180 seconds (due to movement of the mouse being 1 sec each)
+# total time mouse will be moving is 8 hours.
+a = 160
+
+while(a != 0):
+    move_mouse()
+    a = a - 1
+    # Moves mouse every 3 minutes
+    time.sleep(176)
