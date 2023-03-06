848119034982baa955dd82a433a0547d1ffdb91a
raythurman2386 <raymondthurman5@gmail.com>
2023-03-06 12:58:36 -0600

    updated times

--- mouse_keyboard_automation/main.py
+++ mouse_keyboard_automation/main.py
@@ -6,7 +6,7 @@ import time
 # Function that moves the mouse to the 4 corners of the screen
 def move_mouse():
     pg.moveTo(100, 100, duration=1)
-    pg.click(100,100)
+    pg.click(100, 100)
     pg.moveTo(1820, 100, duration=1)
     pg.moveTo(100, 980, duration=1)
     pg.click(100, 980)
@@ -15,9 +15,9 @@ def move_mouse():
 
 # With 160 revolutions over 180 seconds (due to movement of the mouse being 1 sec each)
 # total time mouse will be moving is 8 hours.
-a = 160
+a = 150
 
-while(a != 0):
+while (a != 0):
     move_mouse()
     a = a - 1
     # Moves mouse every 3 minutes
