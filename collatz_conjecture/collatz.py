64d00f3f97ff151cc2f1cba063884e38101b0b4a
raythurman2386 <raymondthurman5@gmail.com>
2023-07-24 11:14:40 -0500

    collatz conjecture python implementation

---
+++ collatz_conjecture/collatz.py
@@ -0,0 +1,28 @@
+def collatz(n, s=None):
+    # Ensure the number is a positive whole number
+    if n <= 0:
+        raise ValueError(f'Please input a value greater than 0! You input {n}.')
+    
+    if type(n) is not int:
+        raise TypeError(f'Please input a valid positive whole number!')
+    
+    # Keep a list of the numbers calculated
+    sequence = [n]
+    
+    # Till the number reaches 1
+    while n != 1:
+        # If the number even, divide by two
+        if n % 2 == 0:
+            n = n // 2
+        else:
+        # If the number is odd, multiply by 3 and add 1
+            n = 3 * n + 1
+            
+        # Add the number to the list
+        sequence.append(n)
+        
+    # return the final list
+    return sequence
+
+print(collatz(391))
+print(collatz(3919377))
