696a3cac68789dd09f05398cd1b126fc8b198eee
raythurman2386 <raymondthurman5@gmail.com>
2023-08-06 10:00:59 -0500

    small optimizations, ui for mouse script

--- mouse_keyboard_automation/main.py
+++ mouse_keyboard_automation/main.py
@@ -1,24 +1,22 @@
-# mouse automation
 import pyautogui as pg
 import time
 
-
 # Function that moves the mouse to the 4 corners of the screen
 def move_mouse():
-    pg.moveTo(100, 100, duration=1)
-    pg.click(100, 100)
-    pg.moveTo(1820, 100, duration=1)
-    pg.moveTo(100, 980, duration=1)
-    pg.click(100, 980)
-    pg.moveTo(1820, 980, duration=1)
-
+    corners = [(100, 100), (1820, 100), (100, 980), (1820, 980)]
+    for x, y in corners:
+        pg.moveTo(x, y, duration=1)
+        pg.click(x, y)
 
 # With 160 revolutions over 180 seconds (due to movement of the mouse being 1 sec each)
 # total time mouse will be moving is 8 hours.
-a = 150
+total_revolutions = 160
+time_per_revolution = 176  # seconds
 
-while (a != 0):
+while total_revolutions > 0:
     move_mouse()
-    a = a - 1
+    total_revolutions -= 1
+
     # Moves mouse every 3 minutes
-    time.sleep(176)
+    time.sleep(time_per_revolution)
+
---
+++ mouse_keyboard_automation/mainui.py
@@ -0,0 +1,46 @@
+import pyautogui as pg
+import time
+import threading
+import tkinter as tk
+
+# Function that moves the mouse to the 4 corners of the screen
+def move_mouse():
+    corners = [(100, 100), (1820, 100), (100, 980), (1820, 980)]
+    for x, y in corners:
+        pg.moveTo(x, y, duration=1)
+        pg.click(x, y)
+
+# Function to start the mouse movement in a separate thread
+def start_movement():
+    global running
+    running = True
+    while running:
+        move_mouse()
+        time.sleep(time_per_revolution)
+
+# Function to stop the mouse movement thread
+def stop_movement():
+    global running
+    running = False
+
+# With 160 revolutions over 180 seconds (due to movement of the mouse being 1 sec each)
+# total time mouse will be moving is 8 hours.
+total_revolutions = 160
+time_per_revolution = 180  # seconds
+
+running = False
+
+# Create the tkinter GUI
+root = tk.Tk()
+root.title("Mouse Movement")
+
+# Start button
+start_button = tk.Button(root, text="Start", command=lambda: threading.Thread(target=start_movement).start())
+start_button.pack(pady=10)
+
+# Stop button
+stop_button = tk.Button(root, text="Stop", command=stop_movement)
+stop_button.pack(pady=10)
+
+# Run the tkinter main loop
+root.mainloop()
